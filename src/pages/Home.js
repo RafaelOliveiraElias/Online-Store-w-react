591ca42db308d1502f21e4236633200c31be56e0
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-03-10 16:52:36 -0300

    2 feita

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    return (<p>Digite algum termo de pesquisa ou escolha uma categoria.</p>);
+    return (
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>);
   }
 }

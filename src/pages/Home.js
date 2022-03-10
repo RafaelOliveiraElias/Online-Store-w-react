65c09e3d77e62c469946537d118e8184d2faa4f9
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-03-10 19:36:19 -0300

    Requisito 3 corrigido

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Categories from '../components/Categories';
+import cartIcon from '../icons/outline_shopping_cart_black_18dp.png';
 
 export default class Home extends Component {
   render() {
     return (
       <>
         <Link to="/cart" data-testid="shopping-cart-button">
-          Carrinho
+          <img
+            src={ cartIcon }
+            alt="Icone do carrinho de compras"
+          />
         </Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.

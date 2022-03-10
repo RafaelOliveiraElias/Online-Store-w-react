5e483c4d8e9f639f3d0bcdd2c04ce2970a5c1a39
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-03-10 16:27:02 -0300

    numero 1 feita, concertada

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import * as api from './services/api';
-
-api.getCategories().then(categories => { console.log(categories) });
-
 function App() {
   return (
     <div className="App">

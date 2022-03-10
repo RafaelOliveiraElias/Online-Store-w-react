3a6f777604fe0bc1951abd33acfb122ac33c7e12
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-03-10 16:24:20 -0300

    numero 1 feita

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as api from './services/api';
+
+api.getCategories().then(categories => { console.log(categories) });
+
 function App() {
   return (
     <div className="App">

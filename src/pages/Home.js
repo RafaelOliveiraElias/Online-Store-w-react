ef4f981ef4a56cea54ee2f6921f07fddc75572b0
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-03-10 16:47:33 -0300

    inicio 2

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (<p>Digite algum termo de pesquisa ou escolha uma categoria.</p>);
+  }
+}
